7a8c87d11e0a4b678ecd148274691d3027c3c98a
Anh Tu Nguyen <anhtunguyen6106@gmail.com>
2025-05-25 23:05:16 +0700

    project

---
+++ collision_detector.py
@@ -0,0 +1,16 @@
+
+class CollisionDetector:
+    
+    @staticmethod
+    def check_collision(bird, pipe, ground_y):
+        bird_rect = bird.get_rect()
+        
+        # Check ground collision
+        if bird.y + bird.height//2 >= ground_y:
+            return True
+        
+        # Check pipe collision
+        top_pipe_rect = pipe.get_top_rect()
+        bottom_pipe_rect = pipe.get_bottom_rect()
+        
+        return bird_rect.colliderect(top_pipe_rect) or bird_rect.colliderect(bottom_pipe_rect)
